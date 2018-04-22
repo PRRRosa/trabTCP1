49179dbe2b5c2bb6cf7f512461bd299a2a2c47b5
David Mees Knijnik <kdavidk98@gmail.com>
2018-04-22 20:30:24 -0300

    começando a implementação de transferência pendente. o Astah também precisa ser alterado

--- bank-full/src/bank/business/domain/Transfer.java
+++ bank-full/src/bank/business/domain/Transfer.java
@@ -7,6 +7,24 @@ package bank.business.domain;
 public class Transfer extends Transaction {
 
 	private CurrentAccount destinationAccount;
+	
+	public enum TransferStatus{
+		FINALIZED,PENDING,CANCELLED
+	}
+	
+	private TransferStatus status;
+	
+	public void setFinalized() {
+		status=TransferStatus.FINALIZED;
+	}
+
+	public void setCancelled() {
+		status=TransferStatus.CANCELLED;
+	}
+	
+	public void setPending() {
+		status=TransferStatus.PENDING;
+	}
 
 	public Transfer(OperationLocation location, CurrentAccount account,
 			CurrentAccount destinationAccount, double amount) {

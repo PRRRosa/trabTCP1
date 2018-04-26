ada975afe5d5ed8bc1c19c55df6b3ed03951b915
PauloRRR <ricardo.paulorr@gmail.com>
2018-04-26 02:03:48 -0300

    implementação de AuthenticateTransactionCommand

--- bank-full/src/bank/ui/text/command/AuthenticateTransactionCommand.java
+++ bank-full/src/bank/ui/text/command/AuthenticateTransactionCommand.java
@@ -3,6 +3,7 @@ package bank.ui.text.command;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Scanner;
 
 import bank.business.AccountOperationService;
 import bank.business.domain.Transfer;
@@ -20,11 +21,13 @@ public class AuthenticateTransactionCommand extends Command{
 	@Override
 	public void execute() throws Exception {
 		// TODO Auto-generated method stub
+		Scanner accountIndex = new Scanner(System.in);
 		List<Transfer> PendingTransfer = accountOperationService.listPendingTransfers();
 		if(PendingTransfer.size()==0) {
 			System.out.println("Nenhuma Pendencia");
 		}
 		else {
+			System.out.println("Escolha o índice da transferência:");
 			for(int i=0;i<PendingTransfer.size();i++) {
 				DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
 				System.out.print(i + ": ");
@@ -32,7 +35,10 @@ public class AuthenticateTransactionCommand extends Command{
 				System.out.print("Data" + ":" + df.format(PendingTransfer.get(i).getDate()) + " ");
 				System.out.println("Localiza��o"+":"+ PendingTransfer.get(i).getAccount().toString());	
 			}
+			
+			int transferIndex = accountIndex.nextInt();
 		}
+		
 	}
 
 }
